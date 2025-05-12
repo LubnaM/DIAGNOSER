de5ea4a5a3b4ccf6bed5223023215f7190f3f32f
Lubna Abu Zohair <45004208+LubnaM@users.noreply.github.com>
2025-05-12 12:29:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def predict():
         result = classifier(input_text)[0]
         label = result['label']
         score = result['score']
-        diagnosis = "Other Abdominal Disease" if label == "LABEL_1" else "Appendicitis"
+        diagnosis = "Appendicitis" if label == "LABEL_1" else "Other Abdominal Disease"
 
         return jsonify({
             "diagnosis": diagnosis,
